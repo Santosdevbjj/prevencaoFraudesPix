286641e1bde350b0e66a7aebc14e37bc33903179
Sergio <santossergiorealbjj@outlook.com>
2025-11-30 01:26:44 -0300

    Create config.py

---
+++ src/utils/config.py
@@ -0,0 +1,4 @@
+DATA_RAW = "data/raw/transactions.parquet"
+DATA_PROCESSED = "data/processed/dataset.parquet"
+MODEL_ARTIFACT = "models/artifacts/model_xgb.joblib"
+REPORT_METRICS = "models/reports/metrics_xgb.json"
